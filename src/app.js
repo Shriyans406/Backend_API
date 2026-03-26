6b8f8188be0931a756223c78b74b50d50e795d46
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-03-26 15:32:36 +0530

    initialized cors but with errors

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+// basic config
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(express.static("public"));
+
+// cors config
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to basecampy!");
+});
+
+export default app;
