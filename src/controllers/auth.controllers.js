9d774239177eb5ced1e201a3e9b1c6e20e1fa00a
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-04-02 17:44:50 +0530

    started registration defination

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,6 +2,8 @@ import { user } from "../models/user.model.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js";
+import { User } from "../models/user.models.js";
 
 const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -37,4 +39,34 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const { unHashedToken, hashedToken, tokenExpiry } =
     user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+  await sendEmail({
+    email: user?.email,
+    subject: "Email Verification",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get("host")}/api/v1/auth/verify-email/${unHashedToken}`,
+    ),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Failed to create user");
+  }
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        201,
+        { user: createdUser },
+        "User registered successfully",
+      ),
+    );
 });
