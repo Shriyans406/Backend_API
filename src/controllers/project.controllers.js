fb5ff8efc32bfedb81dd4b176a6a58f90fd45c23
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-04-15 14:48:42 +0530

    get project

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -7,11 +7,64 @@ import { asyncHandler } from "../utils/async-handler.js";
 import mongoose from "mongoose";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
-const getProjects = asyncHandler(async (req, res) => {})
+const getProjects = asyncHandler(async (req, res) => {
+  const projects = await ProjectMember.aggregate([
+    {
+      $match: {
+        user: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "projects",
+        localField: "projects",
+        foreignField: "_id",
+        as: "projects",
+        pipeline: [
+          {
+            $lookup: {
+              from: "projectmembers",
+              localField: "_id",
+              foreignField: "projects",
+              as: "projectmembers",
+            },
+          },
+          {
+            $addFields: {
+              members: {
+                $size: "$projectmembers",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$project",
+    },
+    {
+      $project: {
+        project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          members: 1,
+          createdAt: 1,
+          createdBy: 1,
+        },
+        role: 1,
+        _id: 0,
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, projects, "Projects fetched successfully"));
+});
 
-const addMembersToProject = asyncHandler(async (req, res) => {})
+const addMembersToProject = asyncHandler(async (req, res) => {});
 const createProject = asyncHandler(async (req, res) => {
-    const { name, description } = req.body;
+  const { name, description } = req.body;
 
   const project = await Project.create({
     name,
@@ -28,11 +81,9 @@ const createProject = asyncHandler(async (req, res) => {
   return res
     .status(201)
     .json(new ApiResponse(201, project, "Project created Successfully"));
-
-
-})
+});
 const updateProject = asyncHandler(async (req, res) => {
-    const { name, description } = req.body;
+  const { name, description } = req.body;
   const { projectId } = req.params;
 
   const project = await Project.findByIdAndUpdate(
@@ -50,9 +101,9 @@ const updateProject = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, project, "Project updated successfully"));
-})
+});
 const deleteMember = asyncHandler(async (req, res) => {
-    const { projectId } = req.params;
+  const { projectId } = req.params;
 
   const project = await Project.findByIdAndDelete(projectId);
   if (!project) {
@@ -61,15 +112,12 @@ const deleteMember = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, project, "Project deleted successfully"));
+});
 
-})
+const getProjectById = asyncHandler(async (req, res) => {});
+const getProjectMembers = asyncHandler(async (req, res) => {});
 
-const getProjectById = asyncHandler(async (req, res) => {})
-const getProjectMembers = asyncHandler(async (req, res) => {})
-
-
-const updateMemberRole = asyncHandler(async (req, res) => {})
-
+const updateMemberRole = asyncHandler(async (req, res) => {});
 
 export {
   addMembersToProject,
